b3ea88ef47310893a610e0d7590b988a42257f05
Dima Shtempelyuk <dimon.shtempelyuk@gmail.com>
2021-04-13 00:20:40 +0200

    Add project files.

---
+++ ListViewProc/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListViewProc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Person.InitPersons();
+            Persons.DataContext = Person.AllPersons;
+        }
+        private void Persons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Person u = ((KeyValuePair<string, Person>)(sender as ListView).SelectedItem).Value;
+            DataContext = u;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Persons.SelectedItem = 0; 
+        }
+
+    }
+}
---
+++ ListViewProc/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace ListViewProc
+{
+    class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+        private string _surname;
+        public string Surname
+        {
+            get => _surname;
+            set
+            {
+                _surname = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Surname"));
+            }
+        }
+        private DateTime _dob;
+        public DateTime Dob
+        {
+            get => _dob;
+            set
+            {
+                _dob = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Dob"));
+            }
+        }
+        private string _rc;
+        public string Rc
+        {
+            get => _rc;
+            set
+            {
+                _rc = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Tuition"));
+            }
+        }
+        public static Dictionary<string, Person> AllPersons = new Dictionary<string, Person>();
+        public static void InitPersons()
+        {
+            AllPersons["AndrejR1ch"] = new Person
+            {
+                Name = "Andrej",
+                Surname = "Zlatý",
+                Dob = new DateTime (2000,01,01),
+                Rc = "440514/0145/8"
+
+            };
+            AllPersons["NikiTank"] = new Person
+            {
+                Name = "Nikita",
+                Surname = "Blatný",
+                Dob = new DateTime(1998,05,13),
+                Rc = "862476/5456/7"
+            };
+            AllPersons["Honzik"] = new Person
+            {
+                Name = "Jan",
+                Surname = "Novák",
+                Dob = new DateTime(2002,03,04),
+                Rc = "238462/3245/9"
+            };
+        }
+    }
+}
